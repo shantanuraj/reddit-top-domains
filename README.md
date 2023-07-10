02567455430733c7a1328d2f73fcb32ead69694a
Shantanu Raj <s@sraj.me>
2023-07-11 00:15:20 +0200

    feat(app): Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# reddit-top-domains
+
+Script to fetch the top domains posted to sub-reddits.
+
+### Usage
+
+```sh
+python reddit.py Subreddit # r/Subreddit works as well
+# or
+./reddit.py r/Subreddit
+```
+
+Will log the 10 most common domains to the console and write the output
+to a file as CSV to `Subreddit-top-10.csv`
---
+++ reddit.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+import os
+import sys
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+import praw
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+user_agent = os.getenv("USER_AGENT")
+
+if len(sys.argv) != 2:
+    print("Usage: python reddit.py <subreddit>")
+    sys.exit(1)
+
+subreddit_name = sys.argv[1].replace("r/", "", 1)
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+)
+
+subreddit = reddit.subreddit(subreddit_name)
+
+domains = []
+for submission in subreddit.new(limit=1000):
+    domain = urlparse(submission.url).netloc
+    domains.append(domain)
+
+counter = Counter(domains)
+
+print(counter.most_common(10))
